b33bc2fd75bc62df57397a6965b827d84714baf3
Jason Breedlove <j@jasonbreedlove.com>
2022-07-03 11:21:26 -0700

    adding material-ui icons

---
+++ src/components/sidebar/Sidebar.component.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sidebar.styles.css";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <TwitterIcon />
+      <h1>Sidebar</h1>
+    </div>
+  );
+}
+
+export default Sidebar;
